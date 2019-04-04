94d9c118885b52d507b26dd20ab9e91bbe264692
Stephen Trewick and Zema Kebede <zema@>
2019-04-04 13:08:30 -0700

    Translate numbers 1 through 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
 
 
 var romanRay = [];
-
+var digitOne = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+var digitTwo
 
 $(document).ready(function(){
   $("form#romanNum").submit(function(event){
     event.preventDefault();
     var number = $("input#inputNumber").val();
-
     var digits = number.split("").reverse();
     for (var i = 0; i < digits.length; i++) {
       var roman = digits[i] * (10**i);
       romanRay.push(roman);
-    if (digits[i] === "5"){
-      $("#translation").append("it's five!");
-    } else if (number === "4") {
-      $("#translation").append("it's four");
-    } else if (number === "9") {
-      $("#translation").append("it's nine");
-    }
+      if (digits[0]) {
+        var result = digitOne[number - 1];
+        console.log(result)
 
+    }
 
 
     }
-  console.log(romanRay)
+
 
     });
 
-  //$("#translation").append(number);
+
   });
