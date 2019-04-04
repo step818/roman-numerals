b1e91f7886b3271dab9055a1619d1c2c10aaa79b
Stephen Trewick and Zema Kebede <stephen@>
2019-04-04 11:50:46 -0700

    Form recognizes whether number is a 4, 5, or 9.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 
 
-var roman = [];
+var romanRay = [];
+
 
 $(document).ready(function(){
   $("form#romanNum").submit(function(event){
     event.preventDefault();
     var number = $("input#inputNumber").val();
-    $("#translation").append(number);
+
     var digits = number.split("").reverse();
     for (var i = 0; i < digits.length; i++) {
       var roman = digits[i] * (10**i);
-      console.log(roman);
+      romanRay.push(roman);
+    if (digits[i] === "5"){
+      $("#translation").append("it's five!");
+    } else if (number === "4") {
+      $("#translation").append("it's four");
+    } else if (number === "9") {
+      $("#translation").append("it's nine");
     }
 
 
 
-
-
-
+    }
+  console.log(romanRay)
 
     });
+
+  //$("#translation").append(number);
   });
---
+++ notes.js
@@ -0,0 +1,6 @@
+if (number === 5){
+  $("#translation").append("it's five!");
+} else if (number === 4) {
+  $("#translation").append("it's four");
+} else if (number === 9) {
+  $("#translation").append("it's nine");
